9de7b8eae21614296675633a6594cc08d144983f
Harry Britton <harrydbritton@gmail.com>
2024-05-21 15:12:59 +0000

    add likes & comments count to post views & serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,9 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     channel_title = serializers.CharField(source='channel.title', read_only=True)
     like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
+
 
 
 
@@ -48,5 +51,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'channel','channel_title',
-            'image_filter','like_id',
+            'image_filter','like_id', 'likes_count', 'comments_count',
         ]
