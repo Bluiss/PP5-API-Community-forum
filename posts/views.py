0900ecb36cff8da8e9df0770cef5ef04affc6602
Harry Britton <harrydbritton@gmail.com>
2024-05-27 15:23:58 +0000

    feat: update post views and searlizers to accept channel title over id

--- posts/views.py
+++ posts/views.py
@@ -5,12 +5,7 @@ from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
-
 class PostList(generics.ListCreateAPIView):
-    """
-    List posts or create a post if logged in
-    The perform_create method associates the post with the logged in user.
-    """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
@@ -23,11 +18,8 @@ class PostList(generics.ListCreateAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        # user feed
         'owner__followed__owner__profile',
-        # users liked posts
         'likes__owner__profile',
-        # user posts 
         'owner__profile',
     ]
     search_fields = [
@@ -46,9 +38,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve a post and edit or delete it if you own it.
-    """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
