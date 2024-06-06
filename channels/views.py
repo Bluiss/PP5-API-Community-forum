1156b2b2bd8ae36e7b811cf20aa6458d68e01c0c
Harry Britton <harrydbritton@gmail.com>
2024-06-06 20:30:02 +0000

    feat; update channel views adding in pk

--- channels/views.py
+++ channels/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Channel
@@ -33,7 +33,9 @@ class ChannelList(APIView):
 
 
 class ChannelDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    lookup_field = 'pk'
