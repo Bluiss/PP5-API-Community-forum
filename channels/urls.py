abfe1a43bcd3885028e550d76a671258400f955e
Harry Britton <harrydbritton@gmail.com>
2024-06-23 15:41:14 +0000

    re add title detail view to channel views

--- channels/urls.py
+++ channels/urls.py
@@ -4,4 +4,5 @@ from channels import views
 urlpatterns = [
     path('channels/', views.ChannelList.as_view(), name='channel-list'),
     path('channels/<int:pk>/', views.ChannelDetail.as_view(), name='channel-detail'),
+    path('channels/title/<str:title>/', views.ChannelDetailByTitle.as_view(), name='channel-detail-by-title'),
 ]
